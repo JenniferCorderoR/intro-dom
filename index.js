5c683b4a7a7ad5d80de632725b76adbc008fb844
JenniferCorderoR <jennifer.factoriaf5@gmail.com~>
2022-12-05 15:53:57 +0100

    added logic to function change

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function change() {
-    // your code goes here
+    console.log(document.querySelector('h1'));
+    console.log(document.querySelector('.titleClassName'));
+    console.log(document.querySelector('#titleIdName'));
+    
+    console.log(document.querySelector('h1').textContent);
+    console.log(document.querySelector('.titleClassName').textContent);
+    console.log(document.querySelector('#titleIdName').textContent);
 }
